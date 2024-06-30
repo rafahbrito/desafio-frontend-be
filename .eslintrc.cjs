bbbf06d3c1c98b21704381968f11c2641a65342d
Rafael Brito <henrique8264@gmail.com>
2024-06-30 14:55:49 -0300

    chore: :wrench: Modificar configuração do eslint.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,7 @@
-module.exports = {
+odule.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['@rocketseat/eslint-config/react'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
